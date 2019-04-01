1a9aece142f0da8dbb38696f158c3d29f0d18852
AlainKRSS <cvt.alain@gmail.com>
2019-04-01 16:02:13 +0200

    fix(): remove loader

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,15 @@
 <template lang="pug">
 .page
   mouse
-  loader
   nuxt
 </template>
 
 <script>
 import Mouse from '@/components/mouse'
-import Loader from '@/components/loader'
 
 export default {
   components: {
-    'mouse': Mouse,
-    'loader': Loader
+    'mouse': Mouse
   }
 }
 </script>
