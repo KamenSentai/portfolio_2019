b636245c12826b19df7e38b339f642ea3d65426c
AlainKRSS <cvt.alain@gmail.com>
2019-04-01 12:30:08 +0200

    feat(): prepare pages

---
+++ components/loader.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  .loader
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~assets/styles/tools/app'
+
+.loader
+  position absolute
+  left 0
+  top 0
+  width 100vw
+  height 100vh
+  background-color light
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,15 @@
 <template lang="pug">
 div
+  //- loader
   nuxt
 </template>
+
+<script>
+import Loader from '@/components/loader'
+
+export default {
+  components: {
+    'loader': Loader
+  }
+}
+</script>
