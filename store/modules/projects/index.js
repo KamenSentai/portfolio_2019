f10403f2238c8aea92a772f6e0e2dd314a69203f
AlainKRSS <cvt.alain@gmail.com>
2019-04-16 17:36:10 +0200

    feat(): add projects

--- store/modules/projects/index.js
+++ store/modules/projects/index.js
@@ -1,7 +1,107 @@
 const state = [
-  { name: "Loyalty", slug: "loyalty" },
-  { name: "Pokedex", slug: "pokedex" },
-  { name: "Scaneat", slug: "scaneat" },
+  {
+    name: "Mars Journey",
+    slug: "mars-journey",
+    date: new Date('December 2017'),
+    role: "Lead developer",
+    tools: [
+      'Pug',
+      'Stylus',
+      'ES6 JavaScript'
+    ],
+    description: "",
+    demo: "",
+    code: "",
+    released: true
+  },
+  {
+    name: "Pokedex",
+    slug: "pokedex",
+    date: new Date('June 2018'),
+    role: "Full-stack developer",
+    tools: [
+      'PHP Slim',
+      'Stylus',
+      'ES6 JavaScript'
+    ],
+    description: "",
+    demo: "",
+    code: "",
+    released: true
+  },
+  {
+    name: "Green Tips",
+    slug: "green-tips",
+    date: new Date('December 2018'),
+    role: "UI/UX designer & JavaScript developer",
+    tools: [
+      'WordPress',
+      'SCSS',
+      'ES6 JavaScript'
+    ],
+    description: "",
+    demo: "",
+    code: "",
+    released: true
+  },
+  {
+    name: "ChatMEAN",
+    slug: "chat-mean",
+    date: new Date('February 2019'),
+    role: "Back-end developer",
+    tools: [
+      'MongoDB',
+      'AngularJS',
+      'Express.js',
+      'Node.js'
+    ],
+    description: "",
+    demo: "",
+    code: "",
+    released: true
+  },
+  {
+    name: "Gaetan Lefebvre",
+    slug: "gaetan-lefebvre",
+    date: new Date('March 2019'),
+    role: "Front-end developer",
+    tools: [
+      'Nuxt.js'
+    ],
+    description: "",
+    demo: "",
+    code: "",
+    released: true
+  },
+  {
+    name: "Scaneat",
+    slug: "scaneat",
+    date: new Date('April 2019'),
+    role: "Full-stack developer",
+    tools: [
+      'Swift',
+      'Firebase'
+    ],
+    description: "",
+    demo: "",
+    code: "",
+    released: true
+  },
+  {
+    name: "Offit",
+    slug: "offit",
+    date: new Date('June 2019'),
+    role: "Front-end developer",
+    tools: [
+      'WordPress',
+      'Timber',
+      'Vue.js'
+    ],
+    description: "",
+    demo: "",
+    code: "",
+    released: false
+  }
 ]
 
 export default {
