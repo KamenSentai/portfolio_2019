20c00f256d5a1d2ef414640193c52f61e3f78286
AlainKRSS <cvt.alain@gmail.com>
2019-04-27 22:59:58 +0200

    fix(): animate frames

--- components/grid.vue
+++ components/grid.vue
@@ -1,23 +1,19 @@
 <template lang="pug">
 .grid
   .grid_card(v-for="(card, index) in cards" :key="index")
-    a(
-      data-mouse="is-reduced"
-      :title="card.name"
-      :href="card.demo !== '' ? card.demo : card.code"
-      target="_blank"
-      rel="noopener"
-    )
-      .grid_frame
+    .grid_frame
+      a.grid_frame_button(
+        data-mouse="is-reduced"
+        :title="card.name"
+        :href="card.demo !== '' ? card.demo : card.code"
+        target="_blank"
+        rel="noopener"
+      )
         img.grid_image(
           data-mouse="is-reduced"
           :src="require(`../assets/images/${card.slug}/0.png`)"
           :alt="card.name"
         )
-        .grid_mask.grid_mask-top(:data-theme="$store.state.color")
-        .grid_mask.grid_mask-bottom(:data-theme="$store.state.color")
-        .grid_mask.grid_mask-left(:data-theme="$store.state.color")
-        .grid_mask.grid_mask-right(:data-theme="$store.state.color")
     h2.grid_title {{ card.name }}
     .grid_links
       push.grid_link(
@@ -53,6 +49,8 @@ export default {
 <style lang="stylus" scoped>
 @import '~assets/styles/tools/app'
 
+$cardSize = grid(4)
+
 .grid
   display flex
   justify-content space-evenly
@@ -64,57 +62,30 @@ export default {
     display flex
     flex-direction column
     align-items center
-    width grid(4)
+    width $cardSize
     margin 0 25px
     margin-bottom 100px
-    overflow hidden
 
   &_frame
-    position relative
     display flex
     justify-content center
     align-items center
     width 100%
-    height auto
-    background-color $black
+    border 2px solid $white
+    transform-origin 50% 50%
     overflow hidden
 
-  &_mask
-    position absolute
-    width 100%
-    height 100%
-    pointer-events none
-    transition background-color 1s $cubic 2s
-
-    &[data-theme="black"]
-      background-color $black
-
-    &[data-theme="white"]
-      background-color $white
-
-    &-top
-      bottom 100%
-      border-bottom 2px solid $white
-
-    &-bottom
-      top 100%
-      border-top 2px solid $white
-
-    &-left
-      right 100%
-
-    &-right
-      left 100%
+    &_button
+      font-size 0
 
   &_image
-    width 100%
+    width $cardSize
     height auto
-    border 2px solid $white
     transition transform 1s $cubic
     will-change transform
 
     &:hover
-      transform scale(.9375)
+      transform scale(1.125)
 
   &_title
     margin-top 50px
--- pages/works.vue
+++ pages/works.vue
@@ -20,7 +20,12 @@ export default {
   transition: {
     mode: 'out-in',
     enter(el, done) {
-      const tl = new TimelineMax({ onComplete: done })
+      const tl = new TimelineMax({
+        onComplete: () => {
+          TweenMax.set('.grid_frame', { clearProps: 'all' })
+          done()
+        }
+      })
 
       if (this.$store.state.prevRoute === '/') {
         tl.fromTo('.navigation_main', 1, { y: - window.innerHeight / 4 }, { y: 0 }, 0)
@@ -28,13 +33,10 @@ export default {
       }
 
       if (this.$store.state.nextRoute === '/works') {
-        tl.fromTo('.grid_title', 1, { yPercent: -12.5, opacity: 0 }, { yPercent: 0, opacity: 1 }, 1)
+        tl.fromTo('.grid_title', 1, { yPercent: -12.5, opacity: 0 }, { yPercent: 0, opacity: 1 }, 1.5)
         tl.fromTo('.grid_links', 1, { yPercent: -12.5, opacity: 0 }, { yPercent: 0, opacity: 1 }, 2)
-        tl.fromTo('.grid_frame', 1, { opacity: 0 }, { opacity: 1 }, 2)
-        tl.fromTo('.grid_mask-left', 1, { right: '50%' }, { right: '100%' }, 2)
-        tl.fromTo('.grid_mask-right', 1, { left: '50%' }, { left: '100%' }, 2)
-        tl.fromTo('.grid_mask-top', 1, { bottom: '50%' }, { bottom: '100%' }, 3)
-        tl.fromTo('.grid_mask-bottom', 1, { top: '50%' }, { top: '100%' }, 3)
+        tl.fromTo('.grid_frame', 1, { scaleY: 0 }, { scaleY: 1 }, 1)
+        tl.fromTo('.grid_frame', 1, { width: 0 }, { width: '100%' }, 2)
       }
     },
     leave(el, done) {
@@ -49,13 +51,10 @@ export default {
       tl.to('.container', 0, { pointerEvents: 'none' }, 0)
 
       if (this.$store.state.prevRoute === '/works') {
-        tl.to('.grid_mask-top', 1, { bottom: '50%' }, 0)
-        tl.to('.grid_mask-bottom', 1, { top: '50%' }, 0)
-        tl.to('.grid_mask-left', 1, { right: '50%' }, 1)
-        tl.to('.grid_mask-right', 1, { left: '50%' }, 1)
-        tl.to('.grid_frame', 0, { opacity: 0 }, 2)
-        tl.to('.grid_title', 1, { yPercent: -12.5, opacity: 0 }, 1)
-        tl.to('.grid_links', 1, { yPercent: -12.5, opacity: 0 }, 1.5)
+        tl.to('.grid_frame', 1, { width: 0 }, 0)
+        tl.to('.grid_frame', 1, { scaleY: 0 }, 1)
+        tl.to('.grid_title', 1, { yPercent: -12.5, opacity: 0 }, .5)
+        tl.to('.grid_links', 1, { yPercent: -12.5, opacity: 0 }, 1)
       }
 
       if (this.$store.state.nextRoute === '/') {
