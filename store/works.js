385c273fc9e91d063e795e62ec5060ae50010062
AlainKRSS <cvt.alain@gmail.com>
2019-04-28 21:30:48 +0200

    fix(store): remove useless data

--- store/works.js
+++ store/works.js
@@ -2,31 +2,12 @@ export const state = () => ([
   {
     name: "Animation Science",
     slug: "animation-science",
-    date: 'October 2017',
-    role: "Front-end developer",
-    tools: [
-      'HTML',
-      'CSS',
-      'JavaScript',
-      'Mathjax.js'
-    ],
-    images: [
-      { src: '0.png', alt: 'Landing screen' },
-      { src: '1.png', alt: 'Translating formulas' }
-    ],
     code: "https://github.com/KamenSentai/Animation_Science",
     demo: "https://animation-science.alain-caovantruong.fr/"
   },
   {
     name: "Clock",
     slug: "clock",
-    date: 'October 2017',
-    role: "Front-end developer",
-    tools: [
-      'HTML',
-      'CSS',
-      'JavaScript'
-    ],
     images: [
       { src: '0.png', alt: 'Clock' }
     ],
@@ -36,71 +17,24 @@ export const state = () => ([
   {
     name: "Canvas Piano",
     slug: "canvas-piano",
-    date: 'December 2017',
-    role: "Front-end developer",
-    tools: [
-      'HTML',
-      'CSS',
-      'ES6 JavaScript'
-    ],
-    images: [
-      { src: '0.png', alt: 'Piano' },
-      { src: '1.png', alt: 'Landing screen' },
-      { src: '2.png', alt: 'Tribute introduction' },
-      { src: '3.png', alt: 'Game' },
-      { src: '4.png', alt: 'Menu' }
-    ],
     code: "https://github.com/KamenSentai/Canvas_Piano",
     demo: "https://canvas-piano.alain-caovantruong.fr/"
   },
   {
     name: "Raining molecules",
     slug: "raining-molecules",
-    date: 'February 2018',
-    role: "Front-end developer",
-    tools: [
-      'HTML',
-      'CSS',
-      'ES6 JavaScript'
-    ],
-    images: [
-      { src: '0.png', alt: 'Raining molecules' }
-    ],
     code: "https://github.com/KamenSentai/Raining_Molecules",
     demo: "https://raining-molecules.alain-caovantruong.fr/"
   },
   {
     name: "Todo list",
     slug: "todo-list",
-    date: 'April 2018',
-    role: "Front-end developer",
-    tools: [
-      'PHP',
-      'Stylus',
-      'ES6 JavaScript'
-    ],
-    images: [
-      { src: '0.png', alt: 'List of tasks' },
-      { src: '1.png', alt: 'Log in' },
-      { src: '2.png', alt: 'Register' },
-      { src: '3.png', alt: 'Empty list' },
-      { src: '4.png', alt: 'Task to add' }
-    ],
     code: "https://github.com/KamenSentai/Todo_List",
     demo: "https://todolist-alaincaovantruong.000webhostapp.com/"
   },
   {
     name: "Loyalty",
     slug: "loyalty",
-    date: 'December 2018',
-    role: "Mobile developer",
-    tools: [
-      'Swift'
-    ],
-    images: [
-      { src: '0.png', alt: 'Cards ordered by category' },
-      { src: '1.png', alt: 'Categories, favorite & additional' }
-    ],
     code: "https://github.com/KamenSentai/Loyalty",
     demo: ""
   }
