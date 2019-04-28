0e5e6954578bea95e9db39e4cd2b7bd20ef7bef8
AlainKRSS <cvt.alain@gmail.com>
2019-04-29 00:12:31 +0200

    fix(component): remove mouse componenet

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,9 @@
 <template lang="pug">
 .page(:data-theme="$store.state.color")
-  mouse(v-if="!isTouchevents")
   nuxt
 </template>
 
 <script>
-import Mouse from '@/components/mouse'
-
 export default {
   head() {
     return {
@@ -14,30 +11,6 @@ export default {
         'data-theme': this.$store.state.color
       }
     }
-  },
-  data() {
-    return {
-      isTouchevents: false
-    }
-  },
-  components: {
-    'mouse': Mouse
-  },
-  computed: {
-    checkTouchevents() {
-      const prefixes = ' -webkit- -moz- -o- -ms- '.split(' ')
-      const mq = query => window.matchMedia(query).matches
-
-      if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) return true
-
-      const query = ['(', prefixes.join('touch-enabled),('), 'heartz', ')'].join('')
-
-      return mq(query)
-    },
-  },
-  beforeMount() {
-    this.isTouchevents = this.checkTouchevents
-    this.$store.commit('TOGGLE_TOUCHEVENTS', this.isTouchevents)
   }
 }
 </script>
