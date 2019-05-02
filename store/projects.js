30a8e897c33bc74206e91f97cc633b27d7f2cc0b
AlainKRSS <cvt.alain@gmail.com>
2019-05-02 02:18:46 +0200

    fix(store): correct spellings

--- store/projects.js
+++ store/projects.js
@@ -31,7 +31,7 @@ export const state = () => ([
     ],
     description: [
       "I worked on this project with comrades for a 5-days front-end development project. Our team decided to explain how a space trip works on this website.",
-      "Alexandre led the project. He decided which data are displayed and he deisgned alongside Julien about UX and UI.",
+      "Alexandre led the project. He decided which data are displayed and he designed alongside Julien about UX and UI.",
       "As the lead developer, I mainly integrated the sketch and I built a mini staircase-like slider framework. Also, I managed the front-end boilerplate.",
       "Justine managed UX by playing 3D musics and sounds with Howler.js library and Amélie added UI experience with parallax."
     ],
@@ -63,7 +63,7 @@ export const state = () => ([
     description: [
       "For a school homework, I had to develop a Pokedex website using Slim Framework.",
       "I decided to build a Pokemon game that the player can catch pokemons and save them in a database. Only caught pokemons are unlocked in the Pokedex.",
-      "Unfortunately, I couldn't find a Pokedex database providing the specific data I needed, exept one which only contains pokemons of the first generation.",
+      "Unfortunately, I couldn't find a Pokedex database providing the specific data I needed, except one which only contains pokemons of the first generation.",
       "I intend to improve this project later by refactoring it."
     ],
     code: "https://github.com/KamenSentai/Pokedex",
@@ -99,10 +99,10 @@ export const state = () => ([
       { src: '4.png', alt: 'List of articles' }
     ],
     description: [
-      "This is a school project on WordPress about environment. Our team had the idea to develop a blog dedicated to Twitter-like tips and articles about environment.",
+      "This is a school project on WordPress about the environment. Our team had the idea to develop a blog dedicated to Twitter-like tips and articles about the environment.",
       "In particular, they are tweet-like post for quick tips, and standard articles for more specific tips.",
       "Théodore led the project with his ideas about which features we need. He also assisted Bryan in UX/UI design.",
-      "Loris intefrated the sketches and Florian is used to develop with WordPress, so we let him all the back-end.",
+      "Loris integrated the sketches and Florian is used to develop with WordPress, so we let him all the back-end.",
       "Everytime before Florian retrieves Loris' integration, I had to reorganize with components. Then, I worked between front-end and back-end by fetching data in JavaScript."
     ],
     code: "https://github.com/KamenSentai/Green-Tips",
@@ -137,7 +137,7 @@ export const state = () => ([
     description: [
       "In class, we built a Stack MEAN boilerplate, then we had to use it to develop a chat for a school project.",
       "The project was a real opportunity to test all the four technologies of the Stack MEAN ecosystem. The final step remaining is using Socket.io in order to make the chat live.",
-      "Jay-min, Assoïe and Olivia basiclly designed all the sketches, Loris integrated them and I built the chat system."
+      "Jay-min, Assoïe and Olivia basically designed all the sketches, Loris integrated them and I built the chat system."
     ],
     code: "https://github.com/KamenSentai/ChatMEAN",
     demo: "",
@@ -174,7 +174,7 @@ export const state = () => ([
     description: [
       "Gaëtan Lefebvre is a 4-year student at HÉTIC while I am a 3-third student. He was looking for a developer for his portfolio and I accepted to help him.",
       "I started to use Vue.js, but I quickly migrated to Nuxt.js framework for SEO reasons.",
-      "It was a long project, from early January to late March, due to plenty of responsive, animations and SEO contraints.",
+      "It was a long project, from early January to late March, due to plenty of responsive, animations and SEO constraints.",
       "This portfolio is awarded with a Honorable Mention and a Mobile Excellence by Awwwards."
     ],
     code: "https://github.com/KamenSentai/Gaetan-Lefebvre_portfolio",
@@ -208,7 +208,7 @@ export const state = () => ([
     description: [
       "At school, we had a 5-days project about mobile : design a mobile application, develop it and build its business model. There was one rule : use Open Food Fact API.",
       "One member of our team has food constraints in their way of life. So, we imagined an application which helps people to easily detect which product is appropriate for a user by scanning it.",
-      "By the end of the five days, we succeeded with the sign up and sign in feature, we managed how to scan a product code, we can fetch data from the API and interact with the databse."
+      "By the end of the five days, we succeeded with the sign up and sign in feature, we managed how to scan a product code, we can fetch data from the API and interact with the database."
     ],
     code: "https://github.com/KamenSentai/ScanEat",
     demo: "",
